da58b634c8cf12e519a43fc08fb9312b45fa187e
Irina Kukuyeva <ikukuyeva@gmail.com>
2019-02-23 20:05:05 -0800

    Class7: Address linter feedback

--- Class7/Speeding-up-computations-Python.py
+++ Class7/Speeding-up-computations-Python.py
@@ -1,3 +1,5 @@
+"""Module to showcase examples of adding type hints to functions."""
+
 from typing import Union
 
 import numpy as np
@@ -5,34 +7,38 @@ import pandas as pd
 
 
 def bin_departure_delays(delay_min: float) -> str:
+    """Fcn to bin flight delays via if-else statements, rather than cut()
+       function, to compare performance of computation in Class 5.
+    """
     if delay_min <= 15:
         return "no_delay"
     elif (delay_min > 15) & (delay_min <= 30):
         return "small_delay"
     elif (delay_min > 30) & (delay_min <= 60):
-        return "medium_delay"        
+        return "medium_delay"
     elif (delay_min > 60) & (delay_min <= 120):
-        return "big_delay"        
+        return "big_delay"
     elif (delay_min > 120):
-        return "compensated_delay"        
+        return "compensated_delay"
     else:
         return "missing_delay"
 
 
-def delays_requiring_compensation(arrival_delay: np.float64, departure_delay: np.float64) -> int:
+def delays_requiring_compensation(arrival_delay: np.float64,
+                                  departure_delay: np.float64) -> int:
     """Fcn to return if arrival and/or departure delay resulted in passenger
        compensation.
-       
+
        Arguments:
            - arrival_delay:   delay in minutes
            - departure_delay: delay in minutes
-       
+
        Returns:
            - number of delays (arrival and or departure) that were delayed
              so long that passenger got compensated
     """
     count = 0
-    if (arrival_delay/60.0 >= 3) | (departure_delay/60.0 >= 2):
+    if (arrival_delay / 60.0 >= 3) | (departure_delay / 60.0 >= 2):
         # If arrival delay is 3+ hours, or if departure delay is 2+ hours:
         count += 1
     return count
@@ -40,15 +46,15 @@ def delays_requiring_compensation(arrival_delay: np.float64, departure_delay: np
 
 def delays_requiring_compensation_vec(
         arrival_delay: Union[pd.Series, np.ndarray],
-        departure_delay: Union[pd.Series, np.ndarray]) ->  Union[pd.Series,
-                                                                 np.ndarray]:
+        departure_delay: Union[pd.Series, np.ndarray]) -> Union[pd.Series,
+                                                                np.ndarray]:
     """Vectorized fcn to return if arrival and/or departure delay resulted in
        passenger compensation.
-       
+
        Arguments:
            - arrival_delay:   vector of flight departure delays, in minutes
            - departure_delay: vector of flight arrival delays, in minutes
-       
+
        Returns:
            - compensated_delays: vector of flights with "1s" when delay
                                  resulted in passenger
